cdf38b5443fb228f986fc6e58ac03106f02f09f8
Abhishek Kaundal <144792445+AbhishekKaundal0052@users.noreply.github.com>
2023-10-06 10:05:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,30 @@ const $team2wickets = document.getElementById("wickets-team2")
 const resetbutton = document.getElementById("reset")
 const strikebutton = document.getElementById("strike")
 
+const strikeAudio = new Audio("https://i1.faceprep.in/prograd-junior/bat%2Bhit%2Bball.mp3");
+const finishedAudio = new Audio("https://i1.faceprep.in/prograd-junior/Ball%2BHit%2BCheer.mp3");
+
 var team1score = 0
 var team2score = 0
 var team1wickets = 0
 var team2wickets = 0
 var turn = 1
 var ballsfaced = 0
+
 function finished() {
+    finishedAudio.play();
     if (team1score > team2score) alert("IND wins");
     if (team2score > team1score) alert("PAK wins");
     if (team2score == team1score) alert("It is another superover!");
 }
+
+resetbutton.onclick = () => {window.location.reload()
+}
+
 const outcomes = [0,1,2,3,4,5,6,"W"]
 
 strikebutton.onclick = () => {
+    strikeAudio.play();
     ballsfaced++;
     if (turn == 1) {
         var score = outcomes[Math.floor(Math.random() * outcomes.length)]
@@ -56,8 +66,8 @@ strikebutton.onclick = () => {
             console.log(score)
             if(ballsfaced == 6 || team2wickets == 2 || team2score>team1score) {
             turn = 3
+            finishedAudio.play()
             finished()
         }
     }
 }
-resetbutton.onclick = () => {window.location.reload()}
